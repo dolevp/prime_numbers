d4e3cc247f65ff044629b33ca95b3e9f7aef45f9
Dolev Pearl <19dolevp@gmail.com>
2020-09-26 19:43:17 +0300

    show_times and hide_times commandline arguments & update README.md to reflect on the new changes

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Prime numbers
 Find prime numbers between minimum and maximum values by comparing 3 different approaches and using the fastest one.
 
-Run configuration:
-`find_prime_numbers.py [-h] [--min START] [--max END] [--show_times SHOW_TIMEIT_RESULTS]`
+Usage: `python find_prime_numbers.py [-h] [--min START] [--max END] [--show_times] [--hide_times]`
 
 Approaches:
 
 * Naive
-* Square root (extension of the Naive approach)
+* Square root (which is a bit of an optimization to the Naive approach)
 * Sieve of eratosthenes
+
+######_Note: both the Naive approach and its extension are using a bit of an optimization, skipping all even numbers (except for 2 which is prime)_
--- find_prime_numbers.py
+++ find_prime_numbers.py
@@ -17,7 +17,7 @@ def get_approaches():
     return {name: val for name, val in approaches.__dict__.items() if callable(val)}
 
 
-def get_execution_time_in_seconds(function_name, start, end):
+def get_average_execution_time(function_name, start, end):
     function_call_string = f'{function_name}({start}, {end})'
     function_import_string = f'from approaches import {function_name}'
 
@@ -26,16 +26,15 @@ def get_execution_time_in_seconds(function_name, start, end):
         setup=function_import_string
     ).timeit(number=TIMEIT_NUMBER_OF_CALLS)
 
-    average_time = total_time_in_seconds / TIMEIT_NUMBER_OF_CALLS
-    return average_time
+    return total_time_in_seconds / TIMEIT_NUMBER_OF_CALLS
 
 
 def print_primes_from_fastest_approach(start, end, show_timeit_results):
     time_by_function = {}
     for function_name, _ in get_approaches().items():
-        average_time = get_execution_time_in_seconds(function_name, start, end)
+        average_time = get_average_execution_time(function_name, start, end)
         time_by_function[function_name] = average_time
-        if show_timeit_results:
+        if show_timeit_results is True:
             print(f"{function_name} took {average_time}(s) on average")
 
     fastest_approach_name = min(time_by_function, key=time_by_function.get)
@@ -57,9 +56,13 @@ if __name__ == "__main__":
                              f' (default: {DEFAULT_PRIME_NUMBERS_END})',
                         default=DEFAULT_PRIME_NUMBERS_END)
 
-    parser.add_argument('--show_times', dest='show_timeit_results', type=bool,
-                        default=DEFAULT_SHOW_TIMES,
-                        help='Show the time it took for each and every approach. (default: true)')
+    parser.add_argument('--show_times', help='Show the time it took for each and every approach',
+                        dest='show_timeit_results', action='store_true')
+
+    parser.add_argument('--hide_times', help='Hide the time it took for each and every approach',
+                        dest='show_timeit_results', action='store_false')
+
+    parser.set_defaults(show_timeit_results=DEFAULT_SHOW_TIMES)
 
     args = parser.parse_args()
     print_primes_from_fastest_approach(args.start, args.end, args.show_timeit_results)
